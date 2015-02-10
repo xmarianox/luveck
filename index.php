d0b2cdad26d5d238eff730eb0f25196882d656d3
Mariano Molina <xsin.crueldadx@gmail.com>
2015-02-09 22:39:17 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html class="no-js" lang="">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
+    <title>luveck</title>
+
+    <link rel="stylesheet" href="css/main.css">
+    
+    <link rel="stylesheet" href="bower_components/fontawesome/css/font-awesome.min.css">
+
+    <script async src="bower_components/modernizr/modernizr.js"></script>
+  </head>
+  <body>
+    <!--[if lt IE 10]>
+      <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    
+    <main>
+      <aside>
+        <h1>Luveck</h1>
+
+        <nav>
+          <ul>
+            <li><a href="" title="Home">Home</a></li>
+            <li><a href="" title="Sobre Luveck">Sobre Luveck</a></li>
+            <li><a href="" title="Productos">Productos</a></li>
+            <li><a href="" title="Certificaciones">Certificaciones</a></li>
+            <li><a href="" title="Investigación">Investigación</a></li>
+            <li><a href="" title="Servicio al cliente">Servicio al cliente</a></li>
+          </ul>
+        </nav>
+
+        <ul class="contactos">
+          <li><a href="" title="Mail"><i class="fa fa-envelope"></i></a></li>
+          <li><a href="" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+          <li><a href="" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+        </ul>
+
+      </aside>
+      
+      <section>
+        <article>item 1</article>
+      </section>
+      
+    </main>
+    <!-- scripts -->
+    <script src="bower_components/jquery/dist/jquery.js"></script>
+    <script src="js/main.js"></script>
+</body>
+</html>
