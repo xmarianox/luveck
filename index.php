9a2b57db4085dae6390696cdf049931ca0385792
Mariano Molina <xsin.crueldadx@gmail.com>
2015-02-13 01:46:18 -0300

    funcionando tabs y animaciones

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
     <title>luveck</title>
 
+    <link rel="stylesheet" href="bower_components/animate.css/animate.min.css">
+
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/font-awesome.min.css">
 
@@ -15,34 +17,63 @@
     <!--[if lt IE 10]>
       <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
-    <main>
+    <main >
       <aside>
         <h1 class="brand_logo">Luveck</h1>
 
         <nav>
-          <ul>
-            <li><a href="" title="Home">Home</a></li>
-            <li><a href="" title="Sobre Luveck" class="active">Sobre Luveck</a></li>
-            <li><a href="" title="Productos">Productos</a></li>
-            <li><a href="" title="Certificaciones">Certificaciones</a></li>
-            <li><a href="" title="Investigación">Investigación</a></li>
-            <li><a href="" title="Servicio al cliente">Servicio al cliente</a></li>
+          <ul class="tabs">
+            <li><a href="#home" title="Home" class="active">Home</a></li>
+            <li><a href="#sobreLuveck" title="Sobre Luveck">Sobre Luveck</a></li>
+            <li><a href="#productos" title="Productos">Productos</a></li>
+            <li><a href="#certificaciones" title="Certificaciones">Certificaciones</a></li>
+            <li><a href="#investigacion" title="Investigación">Investigación</a></li>
+            <li><a href="#servicioClientes" title="Servicio al cliente">Servicio al cliente</a></li>
           </ul>
-        </nav>
+        </nav><!-- navigation -->
 
         <ul class="contactos">
           <li><a href="" title="Mail"><i class="fa fa-envelope"></i></a></li>
           <li><a href="" title="Facebook"><i class="fa fa-facebook"></i></a></li>
           <li><a href="" title="Twitter"><i class="fa fa-twitter"></i></a></li>
-        </ul>
-
+        </ul><!-- .contactos -->
       </aside>
       
-      <div class="content">
+      <div class="content" id="home">
         <section>
           <article class="slide" id="slide-1"></article>
           <article class="slide" id="slide-2"></article>
           <article class="slide" id="slide-3"></article>
+        </section>
+      </div>
+
+      <div class="content" id="sobreLuveck">
+        <section>
+          <p>sobre-luveck</p>
+        </section>
+      </div>
+
+      <div class="content" id="productos">
+        <section>
+          <p>productos</p>
+        </section>
+      </div>
+
+      <div class="content" id="certificaciones">
+        <section>
+          <p>certificaciones</p>
+        </section>
+      </div>
+
+      <div class="content" id="investigacion">
+        <section>
+          <p>investigacion</p>
+        </section>
+      </div>
+
+      <div class="content" id="servicioClientes">
+        <section>
+          <p>servicio-clientes</p>
         </section>
       </div>
 
--- js/main.js
+++ js/main.js
@@ -14,4 +14,35 @@ $(document).ready(function() {
 	// Resize
 	$(window).resize(calcutaleHeight('.slide'));
 
+	// Tabs initial
+	(function(){
+		$('.content').css('display', 'none');
+		var active = $('.tabs').find('a.active');
+		var elem = active.attr('href');
+		$(elem).css('display', 'block');		
+	})();
+	
+	// Custom tabs
+	$('.tabs a').click(function(event) {
+		event.preventDefault();
+		$('.content').css('display', 'none');
+		$('.content').removeClass('animate fadeInRightBig');
+		
+		$('.tabs a').removeClass('active');
+		$(this).toggleClass('active');
+
+		var elementActive = $(this).attr('class');
+		var tab = $(this).attr('href');
+
+		//console.log($(this)[0], elementActive);
+		//$(tab).addClass('animate fadeInRightBig')
+		$(tab).css({
+			display: 'block',
+			width: '-81%'
+		}).animate({
+			width: '81%'
+		}, 1000);
+	});
+
+
 });
