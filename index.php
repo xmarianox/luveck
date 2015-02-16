b8593ad3e21f15103af364bea293ca7e4e3c13fe
Mariano Molina <xsin.crueldadx@gmail.com>
2015-02-16 18:47:37 -0300

    Update view contactenos

--- index.php
+++ index.php
@@ -34,41 +34,96 @@
           <article class="slide" id="slide-2"></article>
           <article class="slide" id="slide-3"></article>
         </section>
-      </div>
+      </div><!-- home -->
 
       <div class="content" id="sobreLuveck">
         <section>
-          <p>sobre-luveck</p>
+          <article>
+            <h1>Quienes somos</h1>
+            <p>Somos una empresa farmacéutica que se encuentra constituida en Estados unidos, nuestra finalidad es la fabricación. promoción, representación y marketing de medicamentos de uso humano. En búsqueda de la excelencia farmacéitucase han desarrollado estándarss de calidad mundial que permite asegurar la efectividad terapéutica de todos nuestros productos. Nuestra motivación principal es brindar una mejor calidad de vida.</p>
+          </article>
         </section>
-      </div>
+      </div><!-- sobreLuveck -->
 
       <div class="content" id="productos">
         <section>
           <p>productos</p>
         </section>
-      </div>
+      </div><!-- productos -->
 
       <div class="content" id="certificaciones">
         <section>
-          <p>certificaciones</p>
+          <article>
+            <h1>Certificados</h1>
+
+            <h2>OMS</h2>
+            <p>Es el organismo de la Organización de las naciones unidas (ONU) especializado en crear, ejecutar y evaluar políticas de prevención, promoción e intervención en salud a nivel mundial. Es labor de la OMS garantizar el acceso a productos farmacéuticos de buena calidad, seguridad y eficacia mediante el programa de pre-evaluación de medicamentos. La OMS pre evalúa los medicamentos de los laboratorios que lo solicitan. El ser una empresa fabricante avalada por tal organismo internacional da a los consumidores una mayor seguridad con respecto a la calidad y eficacia de los productos que dicha organización ofrece.</p>
+          </article>
         </section>
-      </div>
+      </div><!-- certificaciones -->
 
       <div class="content" id="investigacion">
         <section>
           <p>investigacion</p>
         </section>
-      </div>
+      </div><!-- investigacion -->
 
       <div class="content" id="servicioClientes">
         <section>
-          <p>servicio-clientes</p>
+          <article>
+            <div class="col col-4">
+              <h1 class="animated fadeInUp">Contactenos</h1>
+
+              <div class="sucursales active animated fadeInLeft" data-rel="casa-central">
+                <h2>CASA CENTRAL <i class="fa fa-map-marker animated fadeInDown"></i></h2>
+                <p>Lorem ipsum 2832, 9C - Tegucigalpa<br>
+                (504) 2557-0657 / 2731<br>
+                <a href="mailto:Info@luveck.com" title="Info@luveck.com" target="_blank">Info@luveck.com</a></p>
+              </div>
+              
+              <div class="sucursales animated fadeInLeft" data-rel="sucursal-1">
+                <h2>SUCURSAL 1 <i class="fa fa-map-marker animated fadeInDown"></i></h2>
+                <p>Lorem ipsum 2832, 9C - San Pedro Sula<br>
+                (504) 2557-0657 / 2731<br>
+                <a href="mailto:infoSP@luveck.com" title="infoSP@luveck.com" target="_blank">infoSP@luveck.com</a></p>
+              </div>
+
+              <form action="" id="form-contacto" class="animated fadeInLeft">
+                <div class="form-control">
+                  <h2>CONSULTAS</h2>
+                </div>
+                <div class="form-control">
+                  <input type="text" name="nombre-contacto" id="nombre-contacto" placeHolder="Nombre">
+                </div>
+
+                <div class="form-control">
+                  <input type="email" name="email-contacto" id="email-contacto" placeHolder="Correo electronico">
+                </div>
+
+                <div class="form-control">
+                  <textarea name="mensaje-contacto" id="mensaje-contacto" placeHolder="Mensaje"></textarea>
+                </div>
+
+                <div class="form-control">
+                  <button class="btn">Enviar</button>
+                </div>
+              </form>
+            </div><!-- col-4 -->
+
+            <div class="col col-6">
+              <div class="content-mapa animated fadeIn">
+                <div id="map-canvas"></div>
+              </div>
+            </div><!-- col-6 -->
+          </article>
         </section>
-      </div>
+      </div><!-- #servicioClientes -->
 
     </main>
     <!-- scripts -->
     <script src="js/jquery.min.js"></script>
+    <!-- google maps api -->
+    <script src="http://maps.googleapis.com/maps/api/js?v=3"></script>
     <script src="js/main.js"></script>
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -13,8 +13,42 @@ function tabInit() {
 	var elem = active.attr('href');
 	$(elem).css('display', 'block');
 }
+// Initialize Maps
+/*function initialize() {
+	'use strict';
+    var o = {
+        url: "images/map-pin.png",
+        size: new google.maps.Size(32, 46)
+    }, e = {
+        zoom: 17,
+        center: new google.maps.LatLng(19.438238, - 99.188666),
+        disableDefaultUI: !0
+    };
+    map = new google.maps.Map(document.getElementById("map-canvas"), e);
+    var a = new google.maps.Marker({
+        map: map,
+        icon: o,
+        position: map.getCenter()
+    }), t = '<div id="content"><h2>Latitud Polanco</h2><ul><li>Av Ejército Nacional</li><li>Granada, Miguel Hidalgo</li><li>11520 Ciudad de México, Distrito Federal, México</li></ul></div>', r = new google.maps.InfoWindow({
+        content: t
+    });
+    google.maps.event.addListener(a, "click", function() {
+        r.open(map, a)
+    })
+}*/
+function initialize() {
+	'use strict';
+	var mapOptions = {
+	  center: new google.maps.LatLng(-34.397, 150.644),
+	  zoom: 8,
+	  mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map(document.getElementById("map-canvas"),
+	    mapOptions);
+}
+
 // Load
-$(window).load(calcutaleHeight('.slide'), calcutaleHeight('.slider-menu'), tabInit());
+$(window).load(calcutaleHeight('.slide'), calcutaleHeight('.slider-menu'), tabInit(), initialize());
 // Ready
 $(document).ready(function() {
 	'use strict';
@@ -47,13 +81,16 @@ $(document).ready(function() {
 		var elementActive = $(this).attr('class');
 		var tab = $(this).attr('href');
 		//$(tab).addClass('animate fadeInRightBig')
-		$(tab).css({
+		$(tab).css('display', 'block');
+		/*$(tab).css({
 			display: 'block',
 			width: '-81%'
 		}).animate({
 			width: '81%'
-		}, 1000);
+		}, 1000);*/
 	});
 
+	// Init Google Maps
+	//initialize();
 
 });
