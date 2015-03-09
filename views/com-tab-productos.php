eca7db4782acdc8f10363f98b3fbce46ba1906aa
Mariano Molina <xsin.crueldadx@gmail.com>
2015-03-09 20:15:56 -0300

    Update productos

--- views/com-tab-productos.php
+++ views/com-tab-productos.php
@@ -3,7 +3,26 @@
 		<div class="col col-49">
 			<h1 class="animated fadeInUp">Productos</h1>
 
-			<div id="amlodipen-5" class="content-prod active">
+			<div id="amlodipen25mg" class="content-prod active">
+				<h2 class="animated fadeInUp">Amlodipen 2.5mg</h2>
+				<ul class="animated fadeInUp">
+					<li><em>Principio Activo:</em> S(-) Amlodipina Besilato 2.5 mg</li>
+					<li><em>Categoría terapéutica:</em> Antipertensivo</li>
+					<li><em>Indicaciones:</em> Angina de pecho, hipertensión arterial. *Para mayor información descargue el inserto médico.</li>
+					<li><em>Presentación:</em> Caja con 30 tabletas recubiertas</li>
+					<li><em>Dosis:</em> Consulte a su médico</li>
+					<li><em>Disponible en:</em> Honduras y Guatemala.</li>
+				</ul>
+
+				<h2 class="animated fadeInUp">Presentaciones</h2>
+				<ul class="animated fadeInUp">
+					<li>- <a href="">Amlodipen 5 mg</a> - <a href="">Amlodipen D5</a> - <a href="">Amlodipen LS</a> - </li>
+				</ul>
+
+				<a href="javascript:;" class="btn_descargar animated fadeInUp" title="Descargar inserto médico"><span>Descargar inserto médico</span> <span class="shape"><i class="icon"></i></span></a>
+			</div>
+
+			<div id="amlodipen5mg" class="content-prod active">
 				<h2 class="animated fadeInUp">Amlodipen 5mg</h2>
 				<ul class="animated fadeInUp">
 					<li><em>Principio Activo:</em> S(-) Amlodipina Besilato 5 mg</li>
@@ -22,8 +41,8 @@
 				<a href="javascript:;" class="btn_descargar animated fadeInUp" title="Descargar inserto médico"><span>Descargar inserto médico</span> <span class="shape"><i class="icon"></i></span></a>
 			</div>
 
-			<div id="amlodipen-d" class="content-prod">
-				<h2 class="animated fadeInUp">Amlodipen D</h2>
+			<div id="amlodipenD12mg" class="content-prod">
+				<h2 class="animated fadeInUp">Amlodipen D 12mg</h2>
 				<ul class="animated fadeInUp">
 					<li><em>Principio Activo:</em> S(-) Amlodipina + Hidroclorotiazida 2.5/12.5 mg</li>
 					<li><em>Categoría terapéutica:</em> Antipertensivo</li>
@@ -41,7 +60,7 @@
 				<a href="javascript:;" class="btn_descargar animated fadeInUp" title="Descargar inserto médico"><span>Descargar inserto médico</span> <span class="shape"><i class="icon"></i></span></a>
 			</div>
 
-			<div id="amlodipen-d5" class="content-prod">
+			<div id="amlodipenD5mg" class="content-prod">
 				<h2 class="animated fadeInUp">Amlodipen D5</h2>
 				<ul class="animated fadeInUp">
 					<li><em>Principio Activo:</em> S(-) Amlodipina + Hidroclorotiazida 5/12.5 mg</li>
@@ -60,7 +79,7 @@
 				<a href="javascript:;" class="btn_descargar animated fadeInUp" title="Descargar inserto médico"><span>Descargar inserto médico</span> <span class="shape"><i class="icon"></i></span></a>
 			</div>
 
-			<div id="amlodipen-ls" class="content-prod">
+			<div id="amlodipenLS" class="content-prod">
 				<h2 class="animated fadeInUp">Amlodipen LS</h2>
 				<ul class="animated fadeInUp">
 					<li><em>Principio Activo:</em> S(-) Amlodipina losaran potásico 2.5/50 mg</li>
@@ -79,8 +98,22 @@
 				<a href="javascript:;" class="btn_descargar animated fadeInUp" title="Descargar inserto médico"><span>Descargar inserto médico</span> <span class="shape"><i class="icon"></i></span></a>
 			</div>
 			
-			<div id="atenolen" class="content-prod">
-				<h2 class="animated fadeInUp">Atenolen</h2>
+			<div id="atenolen25mg" class="content-prod">
+				<h2 class="animated fadeInUp">Atenolen 25 mg</h2>
+				<ul class="animated fadeInUp">
+					<li><em>Principio Activo:</em> S(-) Atenolol 25 mg</li>
+					<li><em>Categoría terapéutica:</em> Antihipertensivo</li>
+					<li><em>Indicaciones:</em> Angina de pecho, hipertensión arterial. *Para mayor información descargue el inserto médico.</li>
+					<li><em>Presentación:</em> Caja con 30 tabletas recubiertas</li>
+					<li><em>Dosis:</em> Consulte a su médico</li>
+					<li><em>Disponible en:</em> Honduras y Guatemala.</li>
+				</ul>
+				
+				<a href="javascript:;" class="btn_descargar animated fadeInUp" title="Descargar inserto médico"><span>Descargar inserto médico</span> <span class="shape"><i class="icon"></i></span></a>
+			</div>
+
+			<div id="atenolen50mg" class="content-prod">
+				<h2 class="animated fadeInUp">Atenolen 50 mg</h2>
 				<ul class="animated fadeInUp">
 					<li><em>Principio Activo:</em> S(-) Atenolol 50 mg</li>
 					<li><em>Categoría terapéutica:</em> Antihipertensivo</li>
@@ -92,9 +125,23 @@
 				
 				<a href="javascript:;" class="btn_descargar animated fadeInUp" title="Descargar inserto médico"><span>Descargar inserto médico</span> <span class="shape"><i class="icon"></i></span></a>
 			</div>
+
+			<div id="atenolenS50mg" class="content-prod">
+				<h2 class="animated fadeInUp">Atenolen S 50mg</h2>
+				<ul class="animated fadeInUp">
+					<li><em>Principio Activo:</em> S(-) Atenolol S 50 mg</li>
+					<li><em>Categoría terapéutica:</em> Antihipertensivo</li>
+					<li><em>Indicaciones:</em> Angina de pecho, hipertensión arterial. *Para mayor información descargue el inserto médico.</li>
+					<li><em>Presentación:</em> Caja con 30 tabletas recubiertas</li>
+					<li><em>Dosis:</em> Consulte a su médico</li>
+					<li><em>Disponible en:</em> Honduras y Guatemala.</li>
+				</ul>
+				
+				<a href="javascript:;" class="btn_descargar animated fadeInUp" title="Descargar inserto médico"><span>Descargar inserto médico</span> <span class="shape"><i class="icon"></i></span></a>
+			</div>
 			
-			<div id="doloveck" class="content-prod">
-				<h2 class="animated fadeInUp">Doloveck</h2>
+			<div id="doloveck25mg" class="content-prod">
+				<h2 class="animated fadeInUp">Doloveck 25mg</h2>
 				<ul class="animated fadeInUp">
 					<li><em>Principio Activo:</em> Dexketoprofeno trometamol 25 mg</li>
 					<li><em>Categoría terapéutica:</em> Analgésico y Anti inflamatorio.</li>
@@ -106,8 +153,8 @@
 				<a href="javascript:;" class="btn_descargar animated fadeInUp" title="Descargar inserto médico"><span>Descargar inserto médico</span> <span class="shape"><i class="icon"></i></span></a>
 			</div>
 			
-			<div id="soprapen-20" class="content-prod">
-				<h2 class="animated fadeInUp">Soprapen</h2>
+			<div id="soprapen20" class="content-prod">
+				<h2 class="animated fadeInUp">Soprapen 20mg</h2>
 				<ul class="animated fadeInUp">
 					<li><em>Principio Activo:</em> S(-) Pantoprazol Sódico 20 mg</li>
 					<li><em>Categoría terapéutica:</em> Anti úlceras.</li>
@@ -120,8 +167,8 @@
 				<a href="javascript:;" class="btn_descargar animated fadeInUp" title="Descargar inserto médico"><span>Descargar inserto médico</span> <span class="shape"><i class="icon"></i></span></a>
 			</div>
 
-			<div id="soprapen-iv" class="content-prod">
-				<h2 class="animated fadeInUp">Soprapen IV</h2>
+			<div id="soprapenIV" class="content-prod">
+				<h2 class="animated fadeInUp">Soprapen I.V.</h2>
 				<ul class="animated fadeInUp">
 					<li><em>Principio Activo:</em> S(-) Pantoprazol 20 mg</li>
 					<li><em>Categoría terapéutica:</em> Anti úlceras.</li>
@@ -138,37 +185,100 @@
 
 		<div class="col col-51">
             <ul class="list-prod">
-                <li>
-                	<a href="#doloveck" class="currentProd">
+				<li>
+                	<a href="#amlodipen25mg">
                 		<figure>
-                			<img src="images/productos/img-doloveck.png" height="229" width="472" alt="Doloveck" />
-							<span class="borderCurrent"></span>
-                		</figure>
-                	</a>
-                </li>
-
-                <li>
-                	<a href="#atenolen">
-	                	<figure>
-	            			<img src="images/productos/img-atenolen.png" height="229" width="472" alt="Atenolen" />
-							<span class="borderCurrent"></span>
-	            		</figure>
-                	</a>
-                </li>
-
-                <li>
-                	<a href="#amlodipen">
-                		<figure>
-	            			<img src="images/productos/img-amlodipen.png" height="229" width="472" alt="Amlodipen" />
+	            			<img src="images/productos/amlodipen25mg.png" alt="Amlodipen 25mg" />
 							<span class="borderCurrent"></span>
 	            		</figure>
 	            	</a>
                 </li>
 
                 <li>
-                	<a href="#soprapen" id="selector_soprapen">
+                	<a href="#amlodipen50mg">
+                		<figure>
+	            			<img src="images/productos/amlodipen50mg.png" alt="Amlodipen 50mg" />
+							<span class="borderCurrent"></span>
+	            		</figure>
+	            	</a>
+                </li>
+
+                <li>
+                	<a href="#amlodipenD12mg">
+                		<figure>
+	            			<img src="images/productos/amlodipenD12mg.png" alt="Amlodipen D 12mg" />
+							<span class="borderCurrent"></span>
+	            		</figure>
+	            	</a>
+                </li>
+
+                <li>
+                	<a href="#amlodipenD5mg">
+                		<figure>
+	            			<img src="images/productos/amlodipenD5.png" alt="Amlodipen D 5mg" />
+							<span class="borderCurrent"></span>
+	            		</figure>
+	            	</a>
+                </li>
+
+                <li>
+                	<a href="#amlodipenLS">
+                		<figure>
+	            			<img src="images/productos/amlodipenLS.png" alt="Amlodipen LS" />
+							<span class="borderCurrent"></span>
+	            		</figure>
+	            	</a>
+                </li>
+
+                <li>
+                	<a href="#atenolen25mg">
+	                	<figure>
+	            			<img src="images/productos/atenolen25mg" alt="Atenolen 25mg" />
+							<span class="borderCurrent"></span>
+	            		</figure>
+                	</a>
+                </li>
+
+                <li>
+                	<a href="#atenolen50mg">
+	                	<figure>
+	            			<img src="images/productos/atenolen50mg.png" alt="Atenolen 50mg" />
+							<span class="borderCurrent"></span>
+	            		</figure>
+                	</a>
+                </li>
+
+                <li>
+                	<a href="#atenolenS50mg">
+	                	<figure>
+	            			<img src="images/productos/atenolenS50mg.png" alt="Atenolen S 50mg" />
+							<span class="borderCurrent"></span>
+	            		</figure>
+                	</a>
+                </li>
+
+                <li>
+                	<a href="#doloveck25mg" class="currentProd">
+                		<figure>
+                			<img src="images/productos/doloveck25mg.png" alt="Doloveck 25mg" />
+							<span class="borderCurrent"></span>
+                		</figure>
+                	</a>
+                </li>
+
+                <li>
+                	<a href="#soprapen20mg" id="selector_soprapen">
 						<figure>
-	            			<img src="images/productos/img-soprapen.png" height="229" width="472" alt="Soprapen" />
+	            			<img src="images/productos/soprapen20mg.png" alt="Soprapen 20mg" />
+							<span class="borderCurrent"></span>
+	            		</figure>
+                	</a>
+                </li>
+
+                <li>
+                	<a href="#soprapenIV" id="selector_soprapen">
+						<figure>
+	            			<img src="images/productos/soprapenIV.png" alt="Soprapen I.V." />
 							<span class="borderCurrent"></span>
 	            		</figure>
                 	</a>
